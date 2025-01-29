bd55f4080b0a43fbe94562078ca876282d94659d
EmreMaltepe <emremaltepe54@outlook.com>
2025-01-29 22:34:53 +0300

    new

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import "./js/init";
 import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import "izitoast/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
